a32d6d7cb59766e0d2befd0bebd779b2fcd5b4b5
davidson.or@gmail.com <davidson89@users.noreply.github.com>
2012-10-11 00:31:56 +0000

    criando projeto no netbeans

---
+++ Projeto2-Aed2/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: davidson
+ *
+ * Created on 10 de Outubro de 2012, 21:28
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
