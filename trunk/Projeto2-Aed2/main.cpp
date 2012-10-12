f89abd8ac4c440ee071d7b385cfedc3e439c5edc
davidson.or@gmail.com <davidson.or@gmail.com@1d3c2bc3-5ade-022c-9f3d-a044535caf85>
2012-10-12 00:17:24 +0000

    acertando ponteiro  UnionFInd

--- trunk/Projeto2-Aed2/main.cpp
+++ trunk/Projeto2-Aed2/main.cpp
@@ -30,7 +30,7 @@ int main(int argc, char** argv) {
     int size;
     fscanf(arq,"%d", &size);
     cout << "** " << size << " **" << endl;
-    UnionFind unionFind = new UnionFind( size);
+    UnionFind *unionFind = new UnionFind(size);
 //    while(!feof(arq)){
 //        fscanf(arq,"%d","%d", a , b); //Le  a linha e grava e em str
 //        unionFind->union_set_simples(a , b);
