4b7b26887cdc8e9b923cc7ff4c4b92a4cfaef859
rafa_metallica@hotmail.com <rafa_metallica@hotmail.com@1d3c2bc3-5ade-022c-9f3d-a044535caf85>
2012-10-20 00:03:10 +0000

    Identação

--- Projeto2-Aed2/Les.cpp
+++ Projeto2-Aed2/Les.cpp
@@ -1,15 +1,26 @@
 #include "Les.h"
 
+/*
+ * @param
+ * @return
+ */
 Les::Les(int valor) {
     this->valor = valor;
     this->height = 1;
-    this->lesMain= NULL;
-    this->lesNext= NULL;
+    this->lesMain = NULL;
+    this->lesNext = NULL;
 }
 
+/*
+ * @param
+ * @return
+ */
 Les::Les(const Les &orig) {
 }
 
+/*
+ * @param
+ * @return
+ */
 Les::~Les() {
 }
-
