a0d25ebea1ac95010ae85818e379f9e2d8612068
rafa_metallica@hotmail.com <rafa_metallica@hotmail.com@1d3c2bc3-5ade-022c-9f3d-a044535caf85>
2012-10-29 19:29:34 +0000

    Acerto no destrutor. Fim do memory leak.

--- trunk/Projeto2-Aed2/UnionFind.cpp
+++ trunk/Projeto2-Aed2/UnionFind.cpp
@@ -12,7 +12,7 @@ using namespace std;
  * UnionFind - Constructor. Cria o vetor para objetos da classe Les ou Elemento Floresta.
  * @param size, o tamanho do vetor a ser criado.
  * @param floresta, bool que faz distinção entre objetos a serem criados, podendo ser Les ou Elemento Floresta.
-**/
+ **/
 UnionFind::UnionFind(int size, bool floresta) {
     this->size = size;
     this->operPointer = 0;
@@ -29,7 +29,7 @@ UnionFind::UnionFind(int size, bool floresta) {
 /**
  * make_set_floresta - Inicializa todos as instâncias da classe Elemento_Floresta.
  * @param size, a quantidade de instâncias da classe Elemento_Floresta que serão criadas.
-**/
+ **/
 void UnionFind::make_set_floresta(int size) {
     for (int i = 0; i < size; i++) {
         this->floresta[i] = new Elemento_Floresta(i);
@@ -40,7 +40,7 @@ void UnionFind::make_set_floresta(int size) {
 /**
  * make_set - Inicializa todos as instâncias da classe Les, fazendo com que cada uma receba o seu valor e aponte para si mesmo como Head.
  * @param size, a quantidade de instâncias da classe Les que serão criadas.
-**/
+ **/
 void UnionFind::make_set(int size) {
     for (int i = 0; i < size; i++) {
         this->les[i] = new Les(i);
@@ -52,7 +52,7 @@ void UnionFind::make_set(int size) {
 /**
  * find_set - Retorna o ponteiro para o Head do conjunto.
  * @param no, inteiro que representa o valor de um conjunto.
-**/
+ **/
 Les* UnionFind::find_set(int no) {
     return this->les[no]->lesMain;
 }
@@ -61,7 +61,7 @@ Les* UnionFind::find_set(int no) {
  * union_set_simples - Une o conjunto que contém o elemento a no conjunto que contém o elemento b.
  * @param a, elemento do conjunto a ser unido.
  * @param b, elemento do conjunto a ser unido.
-**/
+ **/
 void UnionFind::union_set_simples(int a, int b) {
     Les *auxA = find_set(a);
     Les *auxB = find_set(b);
@@ -89,7 +89,7 @@ void UnionFind::union_set_simples(int a, int b) {
  * union_set_ponderado - Une o menor conjunto no maior conjunto.
  * @param a, elemento do conjunto a ser unido.
  * @param b, elemento do conjunto a ser unido.
-**/
+ **/
 void UnionFind::union_set_ponderado(int a, int b) {
     if (this->les[a]->lesMain->height > this->les[b]->lesMain->height) union_set_simples(b, a);
     else union_set_simples(a, b);
@@ -98,7 +98,7 @@ void UnionFind::union_set_ponderado(int a, int b) {
 /**
  * union_set_floresta - 
  * @param
-**/
+ **/
 void UnionFind::union_set_floresta(int a, int b) {
     Elemento_Floresta *x = find_set_arvore(a);
     Elemento_Floresta *y = find_set_arvore(b);
@@ -112,7 +112,7 @@ void UnionFind::union_set_floresta(int a, int b) {
  * find_set_arvore - 
  * @param
  * @return
-**/
+ **/
 Elemento_Floresta* UnionFind::find_set_arvore(int no) {
     Elemento_Floresta *representante = this->floresta[no]->pai;
     while (representante != representante->pai) {
@@ -126,7 +126,7 @@ Elemento_Floresta* UnionFind::find_set_arvore(int no) {
  * find_set_arvore_ponderado - 
  * @param
  * @return
-**/
+ **/
 Elemento_Floresta* UnionFind::find_set_arvore_ponderado(int no) {
     Elemento_Floresta *representante = this->floresta[no]->pai;
     while (representante != representante->pai) {
@@ -141,7 +141,7 @@ Elemento_Floresta* UnionFind::find_set_arvore_ponderado(int no) {
 /**
  * union_set_floresta_ponderada - 
  * @param
-**/
+ **/
 void UnionFind::union_set_floresta_ponderada(int a, int b) {
     Elemento_Floresta *x = find_set_arvore_ponderado(a);
     Elemento_Floresta *y = find_set_arvore_ponderado(b);
@@ -162,7 +162,7 @@ void UnionFind::union_set_floresta_ponderada(int a, int b) {
  * find_set_arvore_imprecao - 
  * @param
  * @return
-**/
+ **/
 int UnionFind::find_set_arvore_imprecao(int no) {
     Elemento_Floresta *representante = this->floresta[no];
     while (representante != representante->pai) {
@@ -174,7 +174,7 @@ int UnionFind::find_set_arvore_imprecao(int no) {
 /**
  * getUnionsState - Concatena numa string o valor do head de todos os elementos em um determinado estado.
  * @return retorna a string.
-**/
+ **/
 string UnionFind::get_unions_state_floresta() {
     stringstream ss;
     for (int i = 0; i < this->size; i++) {
@@ -186,7 +186,7 @@ string UnionFind::get_unions_state_floresta() {
 /**
  * getUnionsState - Concatena numa string o valor do head de todos os elementos em um determinado estado.
  * @return retorna a string.
-**/
+ **/
 string UnionFind::get_unions_state() {
     stringstream ss;
     for (int i = 0; i < this->size; i++) {
@@ -197,8 +197,17 @@ string UnionFind::get_unions_state() {
 
 /**
  * ~UnionFind - Destructor. Desaloca o ponteiro para o vetor les ou floresta criado no construtor.
-**/
+ **/
 UnionFind::~UnionFind() {
-    if (isFloresta) delete floresta;
-    else delete les;
+    if (isFloresta) {
+        for (int i = 0; i < this->size; i++) {
+            delete this->floresta[i];
+        }
+        delete floresta;
+    } else {
+        for (int i = 0; i < this->size; i++) {
+            delete this->les[i];
+        }
+        delete les;
+    }
 }
